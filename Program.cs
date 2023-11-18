ec8ad6a10776d10d0e1385cd1bf1572884a2d238
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 11:34:27 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+int numb;
+Console.Write("Please enter integer value: ");
+bool is_parsed = Int32.TryParse(Console.ReadLine(), out numb);
+
+if (is_parsed)
+{
+    int sum = 0;
+    
+    for (int i = 1; i <= numb; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"the sum of the numbers from 1 to {numb} is: {sum} ");
+}
+
+else {
+    Console.WriteLine("Invalid value");
+}
